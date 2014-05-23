8c563ab153cf9e9b3166b670c478f2b84ef7416b
Rafael Valverde Martins <rafaelvalverdemartins@MacBook-Pro-de-Rafael.local>
2014-05-23 02:23:09 -0300

    Adding max length to gender

--- person/models.py
+++ person/models.py
@@ -3,10 +3,10 @@ from django.db import models
 GENDER = (('male', 'male'), ('female', 'female'))
 
 class Person (models.Model):
-	facebookid = models.CharField(max_lenght = 30, primary_key = True)
-	username = models.CharField(max_lenght = 30, blank = False, default = '')
-	name = models.CharField(max_lenght = 100, blank = False, default = '')
-	gender = models.CharField(choices = GENDER, blank = False, default = 'male')
+	facebookid = models.CharField(max_length = 30, primary_key = True)
+	username = models.CharField(max_length = 30, blank = False, default = '')
+	name = models.CharField(max_length = 100, blank = False, default = '')
+	gender = models.CharField(choices = GENDER, default = 'male', max_length = 6)
 
 
 	class Meta:
