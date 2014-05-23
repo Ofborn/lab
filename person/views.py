9b409f4c24cda40dc7590bb18ccc53001b6c405d
Rafael Valverde Martins <rafaelvalverdemartins@MacBook-Pro-de-Rafael.local>
2014-05-23 02:25:42 -0300

    Adding facebookGetData function and saving the function return

--- person/views.py
+++ person/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+
 class PersonList (APIView):
 	def get (self, request, format = None):
 		limit = request.GET.get('limit', None)
@@ -16,12 +17,22 @@ class PersonList (APIView):
 		return Response(serializer.data)
 
 	def post (self, request, format = None):
-		# Preciso criar o metodo de adquirir as infos do facebook
-		serializer = PersonSerializer(data = request.DATA)
+		facebookId = request.POST.get('facebookId', None)
+		data = self.getFacebookData(facebookId)
+		serializer = PersonSerializer(data = data)
 		if serializer.is_valid():
 			serializer.save()
 			return Response(serializer.data, status = status.HTTP_201_CREATED)
 		return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+	def getFacebookData (self, pk):
+		import requests
+		param = {'id': pk}
+		data = requests.get('http://graph.facebook.com/', params = param)
+		formatted = data.json()
+		if data.status_code == 200:
+			formatted['facebookid'] = formatted['id']
+		return formatted
 
 
 class PersonDestroy (APIView):
